7802bd8230089f581377ce881fd4d55848e20774
talatsaleh <talatsaleh98@gmail.com>
2023-09-05 22:22:00 +0300

    stablish third app

---
+++ lib/expenses_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ExpensesScreen extends StatefulWidget {
+  const ExpensesScreen({super.key});
+
+  @override
+  State<ExpensesScreen> createState() => _ExpensesScreenState();
+}
+
+class _ExpensesScreenState extends State<ExpensesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
